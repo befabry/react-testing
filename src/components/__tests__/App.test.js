9f19e73fe735e5a8b6680a41e735fe0ca09ca90c
befabry <fabrybenjamin5@gmail.com>
2020-09-18 23:53:31 +0200

    First commit

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "../App";
+
+it("Shows a comment box", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain("Comment Box");
+
+  //Clean up after a test run for performance, else it stacks up
+  ReactDOM.unmountComponentAtNode(div);
+});

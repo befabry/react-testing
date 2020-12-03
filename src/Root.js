268f5ce7f07ef480300bc210a473eba198a4b83b
befabry <fabrybenjamin5@gmail.com>
2020-12-03 19:31:46 +0100

    stateValidator with json-schema

--- src/Root.js
+++ src/Root.js
@@ -4,9 +4,14 @@ import { createStore, applyMiddleware } from "redux";
 //Customs
 import reducers from "reducers";
 import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, stateValidator)
+  );
 
   return <Provider store={store}>{children}</Provider>;
 };
